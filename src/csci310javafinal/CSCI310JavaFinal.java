0f38d11437883438c45eb4565544d8fe83ccb1de
marcopolobrewing <marcopolobrewing@gmail.com>
2016-11-27 21:07:10 -0700

    added main class to run the game missing some code for the board that will be added later on to allow implementation of the game

--- src/csci310javafinal/CSCI310JavaFinal.java
+++ src/csci310javafinal/CSCI310JavaFinal.java
@@ -1,8 +1,26 @@
 package csci310javafinal;
 
-public class CSCI310JavaFinal {
+import static kiss.API.*;
+import java.awt.BorderLayout;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 
+public class CSCI310JavaFinal extends JFrame {
+    JLabel statusbar;
+    
+    public CSCI310JavaFinal() {
+        statusbar = new JLabel(" 0");
+        add(statusbar, BorderLayout.SOUTH);
+        //add code for Board here when its done
+    }
+    
+    public JLabel getStatusBar() {
+        return statusbar;
+    }
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        CSCI310JavaFinal game = new CSCI310JavaFinal();
+        game.setLocationRelativeTo(null);
+        game.setVisible(true);
     }
 }
